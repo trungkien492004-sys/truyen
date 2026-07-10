cce1fdaae38455f0f364be642d2ed11ddb2a302f
trungkien492004-sys <trungkien492004-sys@github.com>
2026-07-10 11:31:53 +0700

    Fix Vercel build crash: remove puppeteer, test files, and scratch scripts

--- get_db_stories.js
+++
@@ -1,8 +0,0 @@
-const supabase = require('./config/supabase');
-
-async function run() {
-  const { data, error } = await supabase.from('stories').select('id, title');
-  if (error) console.error(error);
-  else console.log(data);
-}
-run();
